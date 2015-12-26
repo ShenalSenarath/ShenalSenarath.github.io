d8b5e319889c16205b6f7647dc8fdd32d0d2485c
Shenal Senarath <shenalsenarath@gmail.com>
2015-12-26 19:59:22 +0530

    optimising

--- js/chart.js
+++ js/chart.js
@@ -1,9 +1,1 @@
-$(function() {
-$('.chart').easyPieChart({
-    easing: 'easeOutBounce',
-    onStep: function(from, to, percent) {
-        $(this.el).find('.percent').text(Math.round(percent));
-    }
-});
-var chart = window.chart = $('.chart').data('easyPieChart');
-});
+$(function(){$('.chart').easyPieChart({easing:'easeOutBounce',onStep:function(from,to,percent){$(this.el).find('.percent').text(Math.round(percent));}});var chart=window.chart=$('.chart').data('easyPieChart');});
